7106fa3b69c013bd8524577a3a4507a5abf36631
generalov <alexander.generalov1987@gmail.com>
2024-05-03 20:24:33 +0300

    comments added

--- merge_files.py
+++ merge_files.py
@@ -1,28 +1,27 @@
 
 
-import pprint
 from operator import itemgetter
 
 files = ["1.txt", "2.txt", "3.txt"]
 
-def merge_files(files, file_to_write):
-    res_list = [] # to store dicts with each file infor mation
+def merge_files(files, file_to_write): # function to mearege files content and files information to one file
+    res_list = [] # to store dicts with each file information
     
-    for file in files:
-        counter = 0
-        tmp_dict = {}
-        file_content = ""
+    for file in files: # iterate over each file in list of files
+        counter = 0 # to store number of lines in file
+        tmp_dict = {} # to store file information
+        file_content = "" # to store file content
         with open(file, encoding='utf-8') as f:
             for l in f:
                 counter += 1
                 file_content += l
-        tmp_dict = {"file_name" : file, "line_numbers" : counter, "file_content" : file_content}
-        res_list.append(tmp_dict)
-    res_list = sorted(res_list, key=itemgetter("line_numbers"))
+        tmp_dict = {"file_name" : file, "line_numbers" : counter, "file_content" : file_content} # add file inormation and file content into dict
+        res_list.append(tmp_dict) # add dict to list of dicts
+    res_list = sorted(res_list, key=itemgetter("line_numbers")) # sort list of dicts by dict key 
     
     with open(file_to_write, 'w') as f:
-        for res in res_list:
-            f.write(f"{res['file_name']}\n{res['line_numbers']}\n{res['file_content']}\n")
+        for res in res_list: # iterate over list of dicts
+            f.write(f"{res['file_name']}\n{res['line_numbers']}\n{res['file_content']}\n") # write data from dict into file
     
     return file_to_write
     
